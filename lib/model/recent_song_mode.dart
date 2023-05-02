dc87b39750cc40af7472e0ade87992b6905657e0
abhishekdv1 <abhishekvallecha20@gmail.com>
2023-05-02 20:14:01 +0530

    asm

---
+++ lib/model/recent_song_mode.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class RecentSong {
+  String songName;
+  String artist;
+  RecentSong({
+    required this.songName,
+    required this.artist,
+  });
+}
--- lib/pages/search_results_screen.dart
+++ lib/pages/search_results_screen.dart
@@ -2,6 +2,7 @@ import 'package:airad/widgets/song_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class SearchResultsScreen extends StatefulWidget {
   final List? m;
@@ -29,20 +30,22 @@ class _SearchResultsScreenState extends State<SearchResultsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Vx.black,
+      ),
       body: widget.m == null
           ? const Text('No result')
-          : ListView.builder(
-              itemCount: widget.m?.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 20.0),
-                  child: SongCard(
+          : Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+              child: ListView.builder(
+                itemCount: widget.m?.length,
+                itemBuilder: (context, index) {
+                  return SongCard(
                       songName: widget.m?[index]['name']['\$t'],
-                      songArtist: widget.m?[index]['artist']['\$t']),
-                );
-              },
+                      songArtist: widget.m?[index]['artist']['\$t']);
+                },
+              ),
             ),
     );
   }
